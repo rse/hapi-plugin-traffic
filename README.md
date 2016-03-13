7c71ea96207fd9a63cdb64b31df332abd421eb50
Ralf S. Engelschall <rse@engelschall.com>
2016-03-13 16:57:44 +0100

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# hapi-plugin-traffic
-HAPI plugin for network traffic accounting
+
+hapi-plugin-traffic
+===================
+
+[HAPI](http://hapijs.com/) plugin for network traffic accounting.
+
+<p/>
+<img src="https://nodei.co/npm/hapi-plugin-traffic.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/hapi-plugin-traffic.png" alt=""/>
+
+Installation
+------------
+
+```shell
+$ npm install hapi hapi-plugin-traffic
+```
+
+About
+-----
+
+This is a small plugin for the [HAPI](http://hapijs.com/) server
+framework for accounting the network traffic per request (received/sent
+payload/raw data plus the start/finish time and duration).
+
+ATTENTION: The information about the network traffic is not 100%
+precise, as this plugin has has to partially estimate the numbers
+`recvRaw` (because HAPI comes too late) and `sentPayload` (in case of
+Boom error responses). Hence, the information is precise enough for the
+usual logging purposes, but do not use at least the partially estimated
+numbers `recvRaw` and `sentPayload` for realistic accounting purposes.
+
+Usage
+-----
+
+```js
+server.register(require("hapi-plugin-traffic"))
+[...]
+server.on("tail", (request) => {
+    let traffic = request.traffic()
+    console.log(
+        `recv=${traffic.recvPayload}/${traffic.recvRaw} ` +
+        `sent=${traffic.sentPayload}/${traffic.sentRaw} ` +
+        `start=${new Date(traffic.timeStart)} ` +
+        `finish=${new Date(traffic.timeFinish)} ` +
+        `duration=${traffic.timeDuration}ms`
+    )
+})
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ hapi-plugin-traffic.js
@@ -0,0 +1,129 @@
+/*
+**  hapi-plugin-traffic -- HAPI plugin for network traffic accounting
+**  Copyright (c) 2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  internal dependencies  */
+var Package = require("./package.json")
+
+/*  the HAPI plugin register function  */
+var register = function (server, options, next) {
+    /*  helper function for accounting the traffic on a socket  */
+    var traffic = {}
+    var trafficOnSocket = (id, socket, field) => {
+        var idL = `${id}:${socket._handle.fd}`
+        var idR = `${socket.remoteFamily}:${socket.remoteAddress}:${socket.remotePort}`
+        if (   traffic[idL] === undefined
+            || (typeof traffic[idL] === "object" && traffic[idL].idR !== idR)) {
+            traffic[idL] = { idR: idR, bytes: 0 }
+        }
+        var bytes = socket[field] - traffic[idL].bytes
+        traffic[idL].bytes = socket[field]
+        return bytes
+    }
+
+    /*  decorate the request object  */
+    server.decorate("request", "traffic", (request) => {
+        /*  cruel duck punching hack to get the raw sent traffic at the right time  */
+        request.raw.res.end = ((end) => {
+            return function () {
+                request.plugins.traffic.sentRaw = trafficOnSocket("res", request.raw.res.socket, "bytesWritten")
+                return end.apply(request.raw.res, arguments)
+            }
+        })(request.raw.res.end)
+
+        /*  provide the accounted traffic  */
+        return () => {
+            return request.plugins.traffic
+        }
+    }, { apply: true })
+
+    /*  hook into the start of request processing  */
+    server.ext("onRequest", (request, reply) => {
+        /*  initialize traffic information  */
+        var now = new Date()
+        request.plugins.traffic = {
+            timeStart:    now,
+            timeFinish:   now,
+            timeDuration: 0,
+            recvPayload:  0,
+            recvRaw:      0,
+            sentPayload:  0,
+            sentRaw:      0
+        }
+
+        /*  count the raw received traffic the cruel way, because
+            request.raw.req.socket.bytesRead is always 0 (HAPI comes too late,
+            the data is already read from the socket and hence has to be estimated)  */
+        var req = request.raw.req
+        request.plugins.traffic.recvRaw +=
+            `${req.method} ${req.url} HTTP/${req.httpVersion}\r\n`.length
+        for (var i = 0; i < request.raw.req.rawHeaders.length; i += 2)
+            request.plugins.traffic.recvRaw +=
+                `${req.rawHeaders[i]}: ${req.rawHeaders[i+1]}\r\n`.length
+        request.plugins.traffic.recvRaw += "\r\n".length
+
+        /*  count the raw and payload received traffic  */
+        request.on("peek", (chunk) => {
+            request.plugins.traffic.recvRaw     += chunk.length
+            request.plugins.traffic.recvPayload += chunk.length
+        })
+
+        return reply.continue()
+    })
+
+    /*  hook into the middle of processing  */
+    server.ext("onPreResponse", (request, reply) => {
+        /*  count the payload sent traffic  */
+        if (request.response.isBoom) {
+            /*  special case: Boom error object
+                (we can only estimate the serialization)  */
+            var json = JSON.stringify(request.response.output.payload)
+            request.plugins.traffic.sentPayload += json.length
+        }
+        else {
+            /*  regular case: the standard HAPI response object  */
+            request.response.on("peek", (chunk) => {
+                request.plugins.traffic.sentPayload += chunk.length
+            })
+        }
+        return reply.continue()
+    })
+
+    /*  hook into the processing where lately, when the response was sent  */
+    server.on("response", (request) => {
+        request.plugins.traffic.timeFinish = new Date()
+        request.plugins.traffic.timeDuration =
+            request.plugins.traffic.timeFinish.getTime() -
+            request.plugins.traffic.timeStart.getTime()
+    })
+
+    /*  continue processing  */
+    next()
+}
+
+/*  provide meta-information as expected by HAPI  */
+register.attributes = { pkg: Package }
+
+/*  export register function, wrapped in a plugin object  */
+module.exports = { register: register }
+
